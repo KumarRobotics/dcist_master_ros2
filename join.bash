bfae7fc7ead8c766a3b9c43082a24aff5514fa89
Fernando Cladera <fcladera@fcladera.com>
2024-01-24 14:23:05 -0500

    Fix join.bash

--- join.bash
+++ join.bash
@@ -3,9 +3,11 @@
 # Typical usage: ./join.bash subt
 #
 
-IMG=$(basename $1)
+IMG=kumarrobotics/$(basename $1)
 
 xhost +
 containerid=$(docker ps -aqf "ancestor=${IMG}")
-docker exec --privileged -e DISPLAY=${DISPLAY} -e LINES=`tput lines` -it ${containerid} bash
+docker exec --privileged -e DISPLAY=${DISPLAY} \
+  -e LINES=`tput lines` \
+  -it ${containerid} bash
 xhost -
